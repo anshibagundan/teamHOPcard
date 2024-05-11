31414477c8f05ac03436a1f2e80d0ceba3ce3525
tarakokko3233 <tanahashist1230@outlook.jp>
2024-05-11 09:16:15 +0900

    staticfiles追加

--- HOPcardAPI/settings.py
+++ HOPcardAPI/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', '0.0.0.0','.herokuapp.com']
 
 
 # Application definition
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
