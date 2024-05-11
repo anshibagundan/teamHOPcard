88f9f11b79aff477850f06c1b067bdf797283153
keisukessu <149043402+keisukessu@users.noreply.github.com>
2024-05-11 20:22:01 +0900

    画面遷移

---
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
@@ -0,0 +1,15 @@
+package jp.ac.ritsumei.ise.phy.exp2.is0674hk.dementia_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
