d2a1db6ea2accaa07ca9fe43d277cc334bd4b568
keisukessu <149043402+keisukessu@users.noreply.github.com>
2024-05-12 18:02:06 +0900

    action画面walking画面の作成

--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/Act_select.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/Act_select.java
@@ -2,5 +2,10 @@ package jp.ac.ritsumei.ise.phy.exp2.is0674hk.dementia_quiz;
 
 public class Act_select {
     private int select_diff;
+    private int id;
+    public Act_select(int id , int select_diff){
+        this.id = id;
+        this.select_diff = select_diff;
+    }
     public int getSelect_diff(){return this.select_diff;}
 }
--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,6 +19,8 @@ public class home extends AppCompatActivity {
     private Button easy;
     private Button normal;
     private Button difficult;
+    private FrameLayout act_selectDiff;
+    private FrameLayout quiz_selectDiff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +33,12 @@ public class home extends AppCompatActivity {
         easy=findViewById(R.id.easy);
         normal=findViewById(R.id.normal);
         difficult=findViewById(R.id.difficult);
+        act_selectDiff=findViewById(R.id.act_selectDiff);
+        quiz_selectDiff=findViewById(R.id.quiz_selectDiff);
 
     }
 
-    public void setEasy(View view){
+    public void set_quizEasy(View view){
         Quiz_select data =new Quiz_select(1,1);
         apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
             @Override
@@ -49,11 +54,11 @@ public class home extends AppCompatActivity {
                 Log.e("setEasy","connection_fail");
             }
         });
-        Intent intent = new Intent(this,game_easy.class);
+        Intent intent = new Intent(this, game.class);
         startActivity(intent);
     }
 
-    public void setNormal(View view){
+    public void set_quizNormal(View view){
         Quiz_select data=new Quiz_select(1,2);
         apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
             @Override
@@ -69,11 +74,11 @@ public class home extends AppCompatActivity {
                 Log.e("setNormal","connection_fail");
             }
         });
-        Intent intent = new Intent(this,game_normal.class);
+        Intent intent = new Intent(this,game.class);
         startActivity(intent);
     }
 
-    public void setDifficult(View view){
+    public void set_quizDifficult(View view){
         Quiz_select data=new Quiz_select(1,3);
         apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
             @Override
@@ -89,20 +94,81 @@ public class home extends AppCompatActivity {
                 Log.e("setDifficult","connection_fail");
             }
         });
-        Intent intent = new Intent(this,game_difficult.class);
+        Intent intent = new Intent(this,game.class);
         startActivity(intent);
     }
+
+    public void set_actEasy(View view){
+        Act_select data =new Act_select(1,1);
+        apiService.insertAct_selectData(data).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful()){
+                    Log.d("set_actEasy","success");
+                }else{
+                    Log.e("set_actEasy","fail");
+                }
+            }
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("set_actEasy","connection_fail");
+            }
+        });
+        act_selectDiff.setVisibility(View.GONE);
+        quiz_selectDiff.setVisibility(View.VISIBLE);
+    }
+    public void set_actNormal(View view){
+        Act_select data =new Act_select(1,2);
+        apiService.insertAct_selectData(data).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful()){
+                    Log.d("set_actNormal","success");
+                }else{
+                    Log.e("set_actNormal","fail");
+                }
+            }
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("set_actNormal","connection_fail");
+            }
+        });
+        act_selectDiff.setVisibility(View.GONE);
+        quiz_selectDiff.setVisibility(View.VISIBLE);
+    }
+    public void set_actDifficult(View view){
+        Act_select data =new Act_select(1,3);
+        apiService.insertAct_selectData(data).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.isSuccessful()){
+                    Log.d("set_actDifficult","success");
+                }else{
+                    Log.e("set_actDifficult","fail");
+                }
+            }
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("set_actDifficult","connection_fail");
+            }
+        });
+        act_selectDiff.setVisibility(View.GONE);
+        quiz_selectDiff.setVisibility(View.VISIBLE);
+    }
+
+
+
 
 //    public void home_game_easy(View view){
 //        Intent intent = new Intent(this,game_easy.class);
 //        startActivity(intent);
 //    }
-    public void home_game_normal(View view){
-        Intent intent = new Intent(this,game_normal.class);
-        startActivity(intent);
-    }
-    public void home_game_difficult(View view){
-        Intent intent = new Intent(this,game_difficult.class);
-        startActivity(intent);
-    }
+//    public void home_game_normal(View view){
+//        Intent intent = new Intent(this,game_normal.class);
+//        startActivity(intent);
+//    }
+//    public void home_game_difficult(View view){
+//        Intent intent = new Intent(this,game_difficult.class);
+//        startActivity(intent);
+//    }
 }
