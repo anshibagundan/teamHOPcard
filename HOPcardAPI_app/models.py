5e7b256aef9c3e5af95eb44ed6b758cb5657de95
tarakokko3233 <tanahashist1230@outlook.jp>
2024-05-13 20:44:48 +0900

    quizとactionテーブルに選択肢を格納した

--- HOPcardAPI_app/models.py
+++ HOPcardAPI_app/models.py
@@ -3,10 +3,14 @@ from django.db import models
 class Quiz(models.Model):
     name = models.CharField(max_length=100)
     difficulty = models.IntegerField()
+    sel_1 = models.CharField(max_length=100)
+    sel_2 = models.CharField(max_length=100)
 
 class Action(models.Model):
     name = models.CharField(max_length=100)
     difficulty = models.IntegerField()
+    sel_1 = models.CharField(max_length=100)
+    sel_2 = models.CharField(max_length=100)
 
 class Quiz_select(models.Model):
     select_diff = models.IntegerField()
