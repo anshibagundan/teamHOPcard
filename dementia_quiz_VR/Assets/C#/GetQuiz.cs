fd40c13731c2ae088db956f5ea174fd02a49425e
tarakokko3233 <tanahashist1230@outlook.jp>
2024-05-14 13:03:39 +0900

    マージ修正

--- dementia_quiz_VR/Assets/C#/GetQuiz.cs
+++ dementia_quiz_VR/Assets/C#/GetQuiz.cs
@@ -12,6 +12,8 @@ public class GetQuiz : MonoBehaviour
     private const string difficultyGetUrl = "http://localhost:8000/quiz-selects/";
     private const string baseGetUrl = "https://teamhopcard-aa92d1598b3a.herokuapp.com/quizzes/";
     private int difficulty = 0;
+    private String url = "";
+    private int randomIndex = 0;
 
 
     public void Start()
@@ -51,7 +53,7 @@ public class GetQuiz : MonoBehaviour
             }
         }
 
-        string url = baseGetUrl + "?difficulty=" + difficulty;
+        url = baseGetUrl + "?difficulty=" + difficulty;
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
             webRequest.SetRequestHeader("X-Debug-Mode", "true");
@@ -69,7 +71,7 @@ public class GetQuiz : MonoBehaviour
 
                 if (quizDataArray != null && quizDataArray.Length > 0)
                 {
-                    int randomIndex = UnityEngine.Random.Range(0, quizDataArray.Length);
+                    randomIndex = UnityEngine.Random.Range(0, quizDataArray.Length);
                     Quiz QuizData = quizDataArray[randomIndex];
                     Quizname.text = QuizData.name;
                     Quizsel_1.text = "1: " + QuizData.sel_1;
@@ -80,6 +82,85 @@ public class GetQuiz : MonoBehaviour
                 {
                     Debug.LogWarning("No quiz found.");
                 }
+            }
+        }
+
+    }
+
+    public void FetchData(String LorR)
+    {
+        StartCoroutine(PostData(LorR));
+    }
+
+    private IEnumerator PostData(String LorR)
+    {
+        int quizDataId = 0;
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            webRequest.SetRequestHeader("X-Debug-Mode", "true");
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + webRequest.error);
+                yield break;
+            }
+            else
+            {
+                string json = webRequest.downloadHandler.text;
+
+                Quiz[] quizDataArray = JsonHelper.FromJson<Quiz>(json);
+
+                if (quizDataArray != null && quizDataArray.Length > 0)
+                {
+                    quizDataId = quizDataArray[randomIndex].id;
+                }
+                else
+                {
+                    Debug.LogWarning("No quiz found.");
+                    yield break;
+                }
+            }
+        }
+
+
+        WWWForm form = new WWWForm();
+
+
+        if (quizDataId % 2 == 0)
+        {
+            if (LorR == "R")
+            {
+                form.AddField("cor", "true");
+                form.AddField("quiz", quizDataId);
+            }
+            else if(LorR == "L")
+            {
+                form.AddField("cor", "false");
+                form.AddField("quiz", quizDataId);
+            }
+        }else{
+            if (LorR == "R")
+            {
+                form.AddField("cor", "false");
+                form.AddField("quiz", quizDataId);
+            }
+            else if(LorR == "L")
+            {
+                form.AddField("cor", "true");
+                form.AddField("quiz", quizDataId);
+            }
+        }
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, form))
+        {
+            webRequest.SetRequestHeader("X-Debug-Mode", "true");
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + webRequest.error);
             }
         }
     }
