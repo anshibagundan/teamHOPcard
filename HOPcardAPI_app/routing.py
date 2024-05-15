e334872481b8727dc9722771653cceaebd1889c2
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-05-16 01:52:51 +0900

    Update routing.py

--- HOPcardAPI_app/routing.py
+++ HOPcardAPI_app/routing.py
@@ -1,6 +1,6 @@
 # routing.py
 from django.urls import re_path
-from HOPcardAPI_app import consumers  # フルパスでインポート
+from . import consumers  # フルパスでインポート
 
 websocket_urlpatterns = [
     re_path(r'ws/hop/$', consumers.HOPConsumer.as_asgi()),
