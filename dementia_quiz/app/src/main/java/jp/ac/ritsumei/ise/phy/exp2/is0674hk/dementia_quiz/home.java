e07887330f8d39f1b25d987d2d3cbcea806392a2
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-05-18 13:35:07 +0900

    Update home.java

--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
@@ -9,14 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import retrofit2.Call;
 import retrofit2.Callback;
-
-
 
 public class home extends AppCompatActivity {
 
@@ -37,161 +38,85 @@ public class home extends AppCompatActivity {
         // ApiServiceインスタンスを取得
         apiService = ApiClient.getApiService();
 
-        easy=findViewById(R.id.easy);
-        normal=findViewById(R.id.normal);
-        difficult=findViewById(R.id.difficult);
-        act_selectDiff=findViewById(R.id.act_selectDiff);
-        quiz_selectDiff=findViewById(R.id.quiz_selectDiff);
-        easy.setOnClickListener(v -> startWebSocket_easy());
-        normal.setOnClickListener(v -> startWebSocket_normal());
-        difficult.setOnClickListener(v -> startWebSocket_difficult());
+        easy = findViewById(R.id.easy);
+        normal = findViewById(R.id.normal);
+        difficult = findViewById(R.id.difficult);
+        act_selectDiff = findViewById(R.id.act_selectDiff);
+        quiz_selectDiff = findViewById(R.id.quiz_selectDiff);
+        easy.setOnClickListener(v -> startWebSocket(1));
+        normal.setOnClickListener(v -> startWebSocket(2));
+        difficult.setOnClickListener(v -> startWebSocket(3));
     }
-    //quiz難易度選択すると画面遷移＋json送信
-    private void startWebSocket_easy() {
+
+    // quiz難易度選択すると画面遷移＋json送信
+    private void startWebSocket(int difficulty) {
         Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
         webSocket = client.newWebSocket(request, new WebSocketListener() {
             @Override
             public void onOpen(WebSocket webSocket, okhttp3.Response response) {
                 super.onOpen(webSocket, response);
-                webSocket.send("{\"start\": \"OK\"}");
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("start", "OK");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                webSocket.send(jsonObject.toString());
             }
         });
-        Quiz_select data =new Quiz_select(1,1);
+
+        Quiz_select data = new Quiz_select(1, difficulty);
         apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, retrofit2.Response<Void> response) {
-                if(response.isSuccessful()){
-                    Log.d("setEasy","success");
-                }else{
-                    Log.e("setEasy","fail");
+                if (response.isSuccessful()) {
+                    Log.d("setDifficulty", "success");
+                } else {
+                    Log.e("setDifficulty", "fail");
                 }
             }
+
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
-                Log.e("setEasy","connection_fail");
+                Log.e("setDifficulty", "connection_fail");
             }
         });
+
         Intent intent = new Intent(this, game.class);
         startActivity(intent);
     }
-    private void startWebSocket_normal() {
-        Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
-        webSocket = client.newWebSocket(request, new WebSocketListener() {
-            @Override
-            public void onOpen(WebSocket webSocket, okhttp3.Response response) {
-                super.onOpen(webSocket, response);
-                webSocket.send("{\"start\": \"OK\"}");
-            }
-        });
-        Quiz_select data=new Quiz_select(1,2);
-        apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, retrofit2.Response<Void> response) {
-                if (response.isSuccessful()){
-                    Log.d("setNormal","success");
-                }else {
-                    Log.e("setNormal","fail");
-                }
-            }
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                Log.e("setNormal","connection_fail");
-            }
-        });
-        Intent intent = new Intent(this,game.class);
-        startActivity(intent);
 
-    }
-    private void startWebSocket_difficult() {
-        Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
-        webSocket = client.newWebSocket(request, new WebSocketListener() {
-            @Override
-            public void onOpen(WebSocket webSocket, okhttp3.Response response) {
-                super.onOpen(webSocket, response);
-                webSocket.send("{\"start\": \"OK\"}");
-            }
-        });
-        Quiz_select data=new Quiz_select(1,3);
-        apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, retrofit2.Response<Void> response) {
-                if (response.isSuccessful()){
-                    Log.d("setDifficult","success");
-                }else {
-                    Log.e("setDifficult","fail");
-                }
-            }
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                Log.e("setDifficult","connection_fail");
-            }
-        });
-        Intent intent = new Intent(this,game.class);
-        startActivity(intent);
-    }
-
-
-//アクションの難易度をPOST＋画面遷移
-    public void set_actEasy(View view){
-        Act_select data =new Act_select(1,1);
-        apiService.insertAct_selectData(data).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, retrofit2.Response<Void> response) {
-                if(response.isSuccessful()){
-                    Log.d("set_actEasy","success");
-                }else{
-                    Log.e("set_actEasy","fail");
-                }
-            }
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                Log.e("set_actEasy","connection_fail");
-            }
-        });
-        act_selectDiff.setVisibility(View.GONE);
-        quiz_selectDiff.setVisibility(View.VISIBLE);
-    }
-    public void set_actNormal(View view){
-        Act_select data =new Act_select(1,2);
-        apiService.insertAct_selectData(data).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, retrofit2.Response<Void> response) {
-                if(response.isSuccessful()){
-                    Log.d("set_actNormal","success");
-                }else{
-                    Log.e("set_actNormal","fail");
-                }
-            }
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                Log.e("set_actNormal","connection_fail");
-            }
-        });
-        act_selectDiff.setVisibility(View.GONE);
-        quiz_selectDiff.setVisibility(View.VISIBLE);
-    }
-    public void set_actDifficult(View view) {
-        Act_select data = new Act_select(1, 3);
+    // アクションの難易度をPOST＋画面遷移
+    public void set_actDifficulty(View view, int difficulty) {
+        Act_select data = new Act_select(1, difficulty);
         apiService.insertAct_selectData(data).enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, retrofit2.Response<Void> response) {
                 if (response.isSuccessful()) {
-                    Log.d("set_actDifficult", "success");
+                    Log.d("set_actDifficulty", "success");
                 } else {
-                    Log.e("set_actDifficult", "fail");
+                    Log.e("set_actDifficulty", "fail");
                 }
             }
 
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
-                Log.e("set_actDifficult", "connection_fail");
+                Log.e("set_actDifficulty", "connection_fail");
             }
         });
         act_selectDiff.setVisibility(View.GONE);
         quiz_selectDiff.setVisibility(View.VISIBLE);
     }
+
+    public void set_actEasy(View view) {
+        set_actDifficulty(view, 1);
+    }
+
+    public void set_actNormal(View view) {
+        set_actDifficulty(view, 2);
+    }
+
+    public void set_actDifficult(View view) {
+        set_actDifficulty(view, 3);
+    }
 }
-
-
-
-
