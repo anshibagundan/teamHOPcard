14bd4ff847ed52dec5e8cad853002281c2bb7cab
keisukessu <149043402+keisukessu@users.noreply.github.com>
2024-05-19 04:47:09 +0900

    design修正

--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/history.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/history.java
@@ -2,9 +2,11 @@ package jp.ac.ritsumei.ise.phy.exp2.is0674hk.dementia_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import java.text.SimpleDateFormat;
@@ -18,6 +20,7 @@ public class history extends AppCompatActivity {
     private UserAdapter userAdapter;
     public static ListView userListView;
     public static ArrayAdapter<String> adapter;
+    public FrameLayout popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +36,19 @@ public class history extends AppCompatActivity {
         userAdapter = new UserAdapter(this, users);
         userListView = findViewById(R.id.userListView);
         userListView.setAdapter(userAdapter);
-
+        popup=findViewById(R.id.popup);
     }
 
     // ボタンが押されたときにperとdateを削除するメソッド
     public void clearPerAndDate(View view) {
         databaseHelper.clearPerAndDate();
+        popup.setVisibility(View.VISIBLE);
+    }
+
+    public void history_main(View view){
+        Intent intent =new Intent(this,MainActivity.class);
+        startActivity(intent);
+        popup.setVisibility(View.GONE);
     }
 
 
