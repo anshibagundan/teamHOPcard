4b93908f34e7090481ff2e902e1b1aeb4462f51d
taramanji <131508158+tarakokko3233@users.noreply.github.com>
2024-05-18 13:47:19 +0900

    Update home.java

--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -44,34 +43,43 @@ public class home extends AppCompatActivity {
         difficult = findViewById(R.id.difficult);
         act_selectDiff = findViewById(R.id.act_selectDiff);
         quiz_selectDiff = findViewById(R.id.quiz_selectDiff);
-        easy.setOnClickListener(v -> startWebSocket(1));
-        normal.setOnClickListener(v -> startWebSocket(2));
-        difficult.setOnClickListener(v -> startWebSocket(3));
+
+        // WebSocket接続を確立
+        startWebSocket();
+
+        easy.setOnClickListener(v -> sendDataAndCloseWebSocket(1));
+        normal.setOnClickListener(v -> sendDataAndCloseWebSocket(2));
+        difficult.setOnClickListener(v -> sendDataAndCloseWebSocket(3));
     }
 
-    // quiz難易度選択すると画面遷移＋json送信
-    private void startWebSocket(int difficulty) {
-            Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
-            webSocket = client.newWebSocket(request, new WebSocketListener() {
-        @Override
-        public void onOpen(WebSocket webSocket, okhttp3.Response response) {
-            super.onOpen(webSocket, response);
-            // 接続が確立された後、少し遅延を入れてからデータを送信する
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject jsonObject = new JSONObject();
-                    try {
-                        jsonObject.put("start", "OK");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    webSocket.send(jsonObject.toString());
-                }
-            }, 1000); // 1秒の遅延を入れる
+    // WebSocket接続を確立
+    private void startWebSocket() {
+        Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
+        webSocket = client.newWebSocket(request, new WebSocketListener() {
+            @Override
+            public void onOpen(WebSocket webSocket, okhttp3.Response response) {
+                super.onOpen(webSocket, response);
+                Log.d("WebSocket", "Connected");
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, okhttp3.Response response) {
+                super.onFailure(webSocket, t, response);
+                Log.e("WebSocket", "Connection failed: " + t.getMessage());
+            }
+        });
+    }
+
+    // データを送信してWebSocket接続を閉じる
+    private void sendDataAndCloseWebSocket(int difficulty) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("start", "OK");
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-    });
+        webSocket.send(jsonObject.toString());
+        webSocket.close(1000, null);
 
         Quiz_select data = new Quiz_select(1, difficulty);
         apiService.insertQuiz_selectData(data).enqueue(new Callback<Void>() {
