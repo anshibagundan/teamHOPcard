6e648fa41f21f2fafad7cb46dce02ff32e7cecb8
keisukessu <149043402+keisukessu@users.noreply.github.com>
2024-05-18 13:43:34 +0900

    通信方法変更２

--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/home.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -50,7 +51,6 @@ public class home extends AppCompatActivity {
 
     // quiz難易度選択すると画面遷移＋json送信
     private void startWebSocket(int difficulty) {
-        private void startWebSocket(int difficulty) {
             Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
             webSocket = client.newWebSocket(request, new WebSocketListener() {
         @Override
@@ -69,7 +69,7 @@ public class home extends AppCompatActivity {
                     }
                     webSocket.send(jsonObject.toString());
                 }
-            }, 1000); // 1秒の遅延を入れる（適宜調整してください）
+            }, 1000); // 1秒の遅延を入れる
         }
     });
 
