2b27708730a60cfae7f50a1fc539e2ae95a6e65e
keisukessu <149043402+keisukessu@users.noreply.github.com>
2024-06-06 23:26:24 +0900

    skip

--- dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/game.java
+++ dementia_quiz/app/src/main/java/jp/ac/ritsumei/ise/phy/exp2/is0674hk/dementia_quiz/game.java
@@ -7,13 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,6 +36,9 @@ public class game extends AppCompatActivity {
     private CustomCircleView customCircleView;
     private int quizSize,actSize;
     private TextView nowgame;
+    private WebSocket webSocket;
+    private final OkHttpClient client = new OkHttpClient();
+    private Button skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +52,17 @@ public class game extends AppCompatActivity {
         quiz_text = findViewById(R.id.quiz_text);
         nowgame=findViewById(R.id.nowgame);
         customCircleView = findViewById(R.id.customCircleView);
+        skip=findViewById(R.id.skip);
 
+        // WebSocket接続を確立
+        startWebSocket();
 
 
         act_setText();
         quiz_setText();
         WebSocketClient webSocketClient = new WebSocketClient(customCircleView);
         webSocketClient.start();
+        skip.setOnClickListener(v -> sendDataAndCloseWebSocket());
     }
 
     public void act_setText() {
@@ -160,6 +175,36 @@ public class game extends AppCompatActivity {
             }
         });
 
+    }
+
+    // WebSocket接続を確立
+    private void startWebSocket() {
+        Request request = new Request.Builder().url("wss://teamhopcard-aa92d1598b3a.herokuapp.com/ws/hop/start/").build();
+        webSocket = client.newWebSocket(request, new WebSocketListener() {
+            @Override
+            public void onOpen(WebSocket webSocket, okhttp3.Response response) {
+                super.onOpen(webSocket, response);
+                Log.d("WebSocket", "Connected");
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, okhttp3.Response response) {
+                super.onFailure(webSocket, t, response);
+                Log.e("WebSocket", "Connection failed: " + t.getMessage());
+            }
+        });
+    }
+
+    // データを送信してWebSocket接続を閉じる
+    private void sendDataAndCloseWebSocket() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("quiz", "SKIP");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        webSocket.send(jsonObject.toString());
+        webSocket.close(1000, null);
     }
 
 
